9c45deedf7067fdbbcf315ce9cc502f5781f8449
Mahlatse <morwawarabothata5@gmail.com>
2025-03-22 00:45:42 +0200

    done for today

--- src/pages/components/Book.jsx
+++ src/pages/components/Book.jsx
@@ -1,5 +1,6 @@
 import "../styles/Book.css"
 import React from "react";
+import myImage from '../../assets/my-picture.png'
 
 export default function Book(){
     return(
@@ -19,15 +20,18 @@ export default function Book(){
         {/* Page 1 */}
         <div className="page" id="page1">
           <div className="front-page">
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil magni laudantium
-              beatae quia. Recusandae, fuga quas consectetur perferendis aperiam esse velit veniam
-              ducimus? Quisquam consequatur perferendis quidem quia, recusandae ab!
-            </p>
+          <p className="para-text">
+            Welcome to my life! <br />
+            My name is Mahlatse Rabothata, and I grew up in a small village called Tlhotlhokwe, Ga-Modjadji, in Limpopo.  
+            I am an aspiring software engineer and computational scientist, deeply passionate about leveraging technology to solve real-world problems, particularly in applied science and mathematics.  
+            Beyond coding, I enjoy socializing, making music, and continuously learning new things that challenge my thinking.  
+            My journey is fueled by curiosity, innovation, and the desire to make a meaningful impact through technology.  
+        </p>
+
             <label className="next" htmlFor="checkbox-page1">▶</label>
           </div>
           <div className="back-page">
-            <img src="1.jpg" alt="Page 1" />
+            <img src={myImage} alt="Page 1" className="w-full h-auto object-contain"/>
             <label className="prev" htmlFor="checkbox-page1">◀</label>
           </div>
         </div>
@@ -44,7 +48,7 @@ export default function Book(){
             <label className="next" htmlFor="checkbox-page2">▶</label>
           </div>
           <div className="back-page">
-            <img src="2.jpg" alt="Page 2" />
+        <img src={myImage} alt="Page 2" className="w-full h-auto object-contain" />
             <label className="prev" htmlFor="checkbox-page2">◀</label>
           </div>
         </div>
